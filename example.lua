edebdb55b7b5119ddd302f33547925251c804fc8
Thomas Anw <tomanw.dev@outlook.com>
2025-02-09 02:37:37 +1100

    Reformatted a bunch of stuff

--- example.lua
+++ example.lua
@@ -1,7 +1,7 @@
 local Logger = require('logger')
 
 -- making a new logger object
-local log = Logger:new('example_log.txt', 1)
+local log = Logger:new('example_log.txt', Logger.LEVELS.INFO)
 
 -- logging some stuff
 log:info('Info!')
--- logger.lua
+++ logger.lua
@@ -1,30 +1,25 @@
 local logger = {}
 logger.__index = logger
 
-local levels = { INFO = 1, WARNING = 2, ERROR = 3, DEBUG =4 }
+logger.LEVELS = { INFO = 1, WARNING = 2, ERROR = 3, DEBUG =4 }
 
 function logger:new(log_file, min_level)
     local obj = setmetatable({}, self)
     obj.log_file   = log_file or 'log.txt'
-    obj.min_level  = min_level or levels.INFO
+    obj.min_level  = min_level or self.LEVELS.INFO
     obj.file       = io.open(obj.log_file, 'a')
     return obj
 end
 
 function logger:log(level, message)
-    local numeric_level = levels[level]
-    if not numeric_level then
-        error('Invalid log level:'.. tostring(level))
-    end
+    if self.LEVELS[level] >= self.min_level then
+        local timestamp   = os.date('[%Y-%m-%d %H:%M:%S]')
+        local log_entry   = string.format('%s [%s] %s\n', timestamp, level, message)
 
-    if numeric_level >= self.min_level then
-        local timestamp = os.date("[%Y-%m-%d %H:%M:%S]")
-        local log_entry = string.format("%s [%s] %s\n", timestamp, level, message)
-
-        -- Print to console
+        -- printing to console
         print(log_entry)
 
-        -- Append to log file
+        -- add to log file
         if self.file then
             self.file:write(log_entry)
             self.file:flush()
@@ -44,4 +39,3 @@ function logger:close()
 end
 
 return logger
-
